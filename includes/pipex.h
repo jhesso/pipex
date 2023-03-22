528a00a5c6c2a5501a67a13c99b816d4ffbfb238
jhesso <jhesso.dev@gmail.com>
2023-03-22 03:04:52 +0200

    created main and header file for the project. nothing really done yet tho

---
+++ includes/pipex.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 20:43:32 by jhesso            #+#    #+#             */
+/*   Updated: 2023/03/21 20:52:03 by jhesso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+
+# include "../libft/includes/libft.h"
+
+#endif
