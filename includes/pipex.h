191caa07d04f7e1eb238a8e8151b6d712e3ce138
jhesso <jhesso.dev@gmail.com>
2023-06-08 22:48:05 +0300

    some formatting stuff

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,12 +6,16 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 16:36:48 by jhesso            #+#    #+#             */
-/*   Updated: 2023/06/01 15:10:36 by jhesso           ###   ########.fr       */
+/*   Updated: 2023/06/08 22:47:09 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+/******************************************************************************/
+/*								Includes  									  */
+/******************************************************************************/
 
 # include <fcntl.h>
 # include <unistd.h>
@@ -22,6 +26,10 @@
 # include <errno.h>
 
 # include "../libft/includes/libft.h"
+
+/******************************************************************************/
+/*								Struct  									  */
+/******************************************************************************/
 
 typedef struct s_data
 {
@@ -39,18 +47,33 @@ typedef struct s_data
 	char	*cmd_path;
 }			t_data;
 
-int		error_msg(char *msg, char *msg2, char *errno_str, int erno);
+/******************************************************************************/
+/*								Functions									  */
+/******************************************************************************/
+
+/*------------------------------Initialization--------------------------------*/
+/* init.c */
 t_data	init(int ac, char **av, char **envp);
-void	free_strings(char *str, char **strs);
-void	close_fds(t_data *data);
+
+/* file.c */
 void	get_heredoc(t_data *d);
-void	exit_err(int error_code, t_data *data);
 void	get_input_file(t_data *d);
 void	get_output_file(t_data *d);
+
+/* path.c */
 char	**get_path(char **envp);
 char	*get_cmd(char *cmd, t_data *d);
 
-/* debug */
+/*-------------------------Utils and error handling---------------------------*/
+/* utils.c */
+void	close_fds(t_data *data);
+void	free_strings(char *str, char **strs);
+
+/* error.c */
+void	exit_err(int error_code, t_data *data);
+int		error_msg(char *msg, char *msg2, char *errno_str, int erno);
+
+/* debug.c */
 void	debug_print_data(t_data data);
 
 #endif
